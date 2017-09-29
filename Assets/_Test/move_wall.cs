e23c4e33fa9c603d5265b064d45dcdcad8398402
Sijian Tang <st509@math.rutgers.edu>
2017-09-29 16:24:00 -0400

    Basic Structure 1st version

---
+++ Assets/_Test/move_wall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_wall : MonoBehaviour {
+
+    public float delta;
+
+    private Transform wall;
+
+    // Use this for initialization
+    void Start () {
+        wall = GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        wall.position = wall.position - delta * movement;
+    }
+}
